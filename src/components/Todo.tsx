0dac8fe5c22497977d262e52cad3d5ab4c13b33f
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2023-10-22 12:03:00 +0200

    Add: Needed components

---
+++ src/components/Todo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TodoItems } from "./TodoItems";
+import { List } from "./List";
+
+export const Todo = () => {
+  return (
+    <>
+      <TodoItems />
+      <List />
+    </>
+  );
+};
