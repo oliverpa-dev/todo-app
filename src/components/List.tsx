754a26f97af7e5c903e45e5f001acbefa1286cb1
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2023-10-22 14:41:05 +0200

    Add: itemSlice into list and items

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+// import { TodoItemsData } from "./types";
+import { storeSelector } from "./app/slice/todoSlice";
+import { useAppSelector } from "./app/hooks";
 
-export const List = () => {
-  return <>List</>;
+export const List: React.FC = () => {
+  const items = useAppSelector(storeSelector);
+  return (
+    <div className="todo-items">
+      <div className="todo-items__heading">Your list of todos</div>
+      {items.map((item) => (
+        <div className="todo-items__copy">
+          <div className="todo-items__copy__title">{item.title}</div>
+          <div className="todo-items__copy__description">
+            {item.description}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import "./todoitems.scss";
+import { addItem } from "./app/slice/todoSlice";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
 
-export const TodoItems = () => {
-  return <>TodoItems</>;
+export const TodoItems: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className="todo-items">
+      <div className="todo-items__heading">Add a new todo</div>
+      <div className="todo-items__copy">
+        <label>Fill in the title</label>
+        <input type="text" />
+      </div>
+      <div className="todo-items__copy">
+        <label>Feel in the description</label>
+        <input type="text" />
+      </div>
+      <button onClick={() => dispatch(addItem())}>Add</button>
+    </div>
+  );
 };
