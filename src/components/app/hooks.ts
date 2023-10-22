fc4947f6821fdb089ee90a1dac4d1f6a2fc3b036
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2023-10-22 14:39:36 +0200

    Add: Custom hooks for dispatch and the main state

---
+++ src/components/app/hooks.ts
@@ -0,0 +1,6 @@
+import React from "react";
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
+import { AppDispatch, RootState } from "./store";
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
