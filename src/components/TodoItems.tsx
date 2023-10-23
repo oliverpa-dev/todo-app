02715cd6c14929f115d41d0648cbb2e46f2c6b70
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2023-10-23 20:40:17 +0200

    Add: states to handle the input and button to send

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./todoitems.scss";
 import { addItem } from "./app/slice/todoSlice";
-import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { useAppDispatch } from "./app/hooks";
+import { InitialState } from "./app/slice/todoSlice";
 
 export const TodoItems: React.FC = () => {
   const dispatch = useAppDispatch();
+
+  const [firstValue, setFirstValue] = useState<string>("");
+  const [secondValue, setSecondValue] = useState<string>("");
+
+  const initialValue: InitialState = {
+    title: firstValue,
+    description: secondValue,
+    id: Math.random(),
+  };
 
   return (
     <div className="todo-items">
       <div className="todo-items__heading">Add a new todo</div>
       <div className="todo-items__copy">
         <label>Fill in the title</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={firstValue}
+          onChange={(e) => {
+            setFirstValue(e.target.value);
+          }}
+        />
       </div>
       <div className="todo-items__copy">
         <label>Feel in the description</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={secondValue}
+          onChange={(e) => {
+            setSecondValue(e.target.value);
+          }}
+        />
       </div>
-      <button onClick={() => dispatch(addItem())}>Add</button>
+      <button onClick={() => dispatch(addItem(initialValue))}>Add</button>
     </div>
   );
 };
