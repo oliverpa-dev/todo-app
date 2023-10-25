228ac45b45e4cb15c5a3329b296712008128e44a
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2023-10-25 20:29:02 +0200

    Add: func for handling the input's onChange

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -16,6 +16,24 @@ export const TodoItems: React.FC = () => {
     id: Math.random(),
   };
 
+  const handleFirstVal = (firstVal: string) => {
+    if (firstVal) {
+      setFirstValue(firstVal);
+      console.log(firstVal);
+    } else {
+      setFirstValue("");
+    }
+  };
+
+  const handleSecondVal = (secondVal: string) => {
+    if (secondVal) {
+      setSecondValue(secondVal);
+      console.log(secondVal);
+    } else {
+      setSecondValue("");
+    }
+  };
+
   return (
     <div className="todo-items">
       <div className="todo-items__heading">Add a new todo</div>
@@ -25,7 +43,8 @@ export const TodoItems: React.FC = () => {
           type="text"
           value={firstValue}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setFirstValue(e.target.value);
+            // setFirstValue(e.target.value);
+            handleFirstVal(e.target.value);
           }}
         />
       </div>
@@ -35,11 +54,18 @@ export const TodoItems: React.FC = () => {
           type="text"
           value={secondValue}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setSecondValue(e.target.value);
+            // setSecondValue(e.target.value);
+            handleSecondVal(e.target.value);
           }}
         />
       </div>
       <button onClick={() => dispatch(addItem(initialValue))}>Add</button>
+      <button
+        onClick={() => {
+          handleFirstVal(firstValue);
+          handleSecondVal(secondValue);
+        }}
+      ></button>
     </div>
   );
 };
