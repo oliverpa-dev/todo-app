a6ac6bb85ebd1a71c2905ef3b3be8895908e39d7
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2023-10-28 12:14:25 +0200

    Add: padding to the content component

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Paper } from "@mui/material";
+import "./main.scss";
 
 interface ContentProps {
   children: React.ReactNode;
 }
 
 export const Content: React.FC<ContentProps> = ({ children }) => {
-  return <Paper>{children}</Paper>;
+  return (
+    <div className="todo-content">
+      <Paper style={{ padding: "5rem" }}>{children}</Paper>
+    </div>
+  );
 };
