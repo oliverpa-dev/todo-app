3bccbda7d105e53e8b36b462a9fb34fec9177d8d
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2023-10-31 20:19:03 +0100

    Add: list layout

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { storeSelector } from "../app/slice/todoSlice";
 import { useAppSelector } from "../app/hooks";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import "./main.scss";
 
 export const List: React.FC = () => {
   const items = useAppSelector(storeSelector);
   return (
-    <div className="todo-items">
-      <div className="todo-items__heading">Your list of todos</div>
+    <div className="todo-list">
+      <div className="todo-list__heading">Your list of todos</div>
       {items.map((item) => (
-        <div key={item.id} className="todo-items__copy">
-          <div className="todo-items__copy__title">{item.title}</div>
-          <div className="todo-items__copy__description">
-            {item.description}
-          </div>
+        <div key={item.id} className="todo-list__copy">
+          <div className="todo-list__copy__title">{item.title}</div>
+          <div className="todo-list__copy__description">{item.description}</div>
+          <IconButton edge="end" aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
         </div>
       ))}
     </div>
